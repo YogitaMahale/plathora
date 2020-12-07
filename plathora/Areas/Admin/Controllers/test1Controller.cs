643340c3d74fe63f10f78440d8e237a4c638eab4
Yogita Mahele <mahaleyogita233@gmail.com>
2020-12-07 18:30:07 +0530

    up

--- plathora/Areas/Admin/Controllers/test1Controller.cs
+++ plathora/Areas/Admin/Controllers/test1Controller.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using appFoodDelivery.Services.Implementation;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using plathora.Entity;
+using plathora.Models;
 using plathora.Services;
 
 namespace plathora.Controllers
@@ -40,5 +43,53 @@ namespace plathora.Controllers
             
             return "complete";
         }
+        [HttpPost]
+        public string MobileView(string mobileNo)
+        {
+            String no = null;
+            Random random = new Random();
+            for (int i = 0; i < 1; i++)
+            {
+                int n = random.Next(0, 999);
+                no += n.ToString("D4") + "";
+            }
+            #region "sms"
+            try
+            {
+
+                string Msg = "OTP :" + no + ".  Please Use this OTP.This is usable once and expire in 10 minutes";
+
+                string OPTINS = "STRLIT";
+
+                string type = "3";
+                string strUrl = "https://www.bulksmsgateway.in/sendmessage.php?user=ezacus&password=" + "ezacus@2020" + "&message=" + Msg.ToString() + "&sender=" + OPTINS + "&mobile=" + mobileNo + "&type=" + 3;
+
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                System.Net.WebRequest request = System.Net.WebRequest.Create(strUrl);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream s = (Stream)response.GetResponseStream();
+                StreamReader readStream = new StreamReader(s);
+                string dataString = readStream.ReadToEnd();
+                response.Close();
+                s.Close();
+                readStream.Close();
+                //    Response.Write("Sent");
+            }
+
+            catch
+            { }
+            #endregion
+
+            return "complete";
+        }
+
+        [HttpPost]
+        public IActionResult MobileView1(string mobileNo)
+        {
+
+            return View();
+        }
+
     }
 }
