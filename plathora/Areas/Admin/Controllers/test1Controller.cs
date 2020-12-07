a4fb5f21e78ce8255c9f67d7c596b769450db2ef
Yogita Mahele <mahaleyogita233@gmail.com>
2020-12-07 13:21:32 +0530

    changes

--- plathora/Areas/Admin/Controllers/test1Controller.cs
+++ plathora/Areas/Admin/Controllers/test1Controller.cs
@@ -1,22 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using appFoodDelivery.Services.Implementation;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using plathora.Entity;
+using plathora.Services;
 
 namespace plathora.Controllers
 {
     [Area("Admin")]
     public class test1Controller : Controller
     {
+        private ISP_Call _sP_Call;
+        private readonly ISubscribeServices _subscribeServices;
+        public test1Controller(ISubscribeServices subscribeServices, ISP_Call sP_Call)
+        {
+            _sP_Call = sP_Call;
+            _subscribeServices = subscribeServices;
+        }
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public int Add(int number1, int number2)
+        public string  Add(string email)
         {
-            return number1 + number2;
+            Subscribe obj = new Subscribe();
+            obj.id = 0;
+            obj.Email = email;
+
+
+            var parameter = new DynamicParameters();
+            parameter.Add("@Email", email);
+
+            _sP_Call.Execute("SubscribeSave", parameter);
+            
+            return "complete";
         }
     }
 }
